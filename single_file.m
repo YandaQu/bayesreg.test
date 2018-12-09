c95cadbd960b4d387247339fbde1b9ac8748787b
Yanda Qu <yqu34@student.monash.edu>
2018-12-09 23:10:11 +1100

    add comparison between two methods

--- single_file.m
+++ single_file.m
@@ -1,11 +1,13 @@
-location = '..\data\Audit data\audit_risk.csv';
+location = 'C:\Users\yanda\Downloads\sparse.csv';
 data = csvread(location, 1);
 data_size = size(data);
 
-acc_10repeats = zeros(1,1);
-for n = 1:1
+acc_10repeats = zeros(10,1);
+acc_10lasso = zeros(10,1);
+for n = 1:10
     cv = cvpartition(data_size(1), 'KFold', 10);
     acc_10folds = zeros(10,1);
+    acc_lasso = zeros(10,1);
     for i = 1:10 
         data_train_X = data(cv.training(i),1:(data_size(2)-1));
         col_left = (var(data_train_X) ~= 0);
@@ -14,15 +16,31 @@ for n = 1:1
         data_test_X = data(cv.test(i),1:(data_size(2)-1));
         data_test_X = data_test_X(:,col_left);
         data_test_y = data(cv.test(i),data_size(2));
-
-        [beta, beta0, retval] = bayesreg(data_train_X, data_train_y, 'logistic', 'horseshoe', 'display',false);
+        
+        % bayesian
+        [beta, beta0, retval] = bayesreg(data_train_X, data_train_y, 'logistic', 'lasso', 'nsamples',1e4,'display',false);
         pred_test_y = br_predict(data_test_X, beta, beta0, retval);
         
         test_size = size(data_test_y);
         acc = sum((pred_test_y.yhat == data_test_y))/test_size(1);
         acc_10folds(i) = acc;
+        
+        % lassoglm
+        [B,FitInfo] = lassoglm(data_train_X, data_train_y,'binomial','CV',3);
+        idxLambdaMinDeviance = FitInfo.IndexMinDeviance;
+        B0 = FitInfo.Intercept(idxLambdaMinDeviance);
+        coef = [B0; B(:,idxLambdaMinDeviance)];
+        yhat = glmval(coef, data_test_X,'logit');
+        yhatBinom = (yhat>=0.5);
+        
+        acc = sum((yhatBinom == data_test_y))/test_size(1);
+        acc_lasso(i) = acc;
     end
     acc_current = mean(acc_10folds);
     acc_10repeats(n) = acc_current;
+    
+    acc_current_lasso = mean(acc_lasso);
+    acc_10lasso(n) = acc_current_lasso;
 end
 disp(mean(acc_10repeats))
+disp(mean(acc_10lasso))
